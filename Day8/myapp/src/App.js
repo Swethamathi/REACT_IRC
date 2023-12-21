71ab1bc06ab5cb76ecdee7350fa0cfcd83800873
Swethamathi <swethamathi21@gmail.com>
2023-12-21 22:14:05 +0530

    dashboard

--- Day8/myapp/src/App.js
+++ Day8/myapp/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import'.Assets/Style.css'
-import WithLoading from './Components/WithLoading'
-import MyComponent from'./Components/MyComponent'
+import {Routes,Route} from 'react-router-dom'
+import  Users from './Pages/Users'
+import Adduser from './Pages/Adduser'
+import Edituser from './Pages/Edituser'
+const App=() =>{
+  return (
+    <>
+    <Routes>
+    <Routes path='/'element={<Users/>}/>
+    <Routes path='/Adduser'element={<Adduser/>}/>
+    <Routes path='/Edituser'element={<Edituser/>}/>
+    </Routes>
 
-const WrappedComponentWithLoading
+    </>
+  )
+}
 
 export default App
--- myapp/src/Components/Classcomponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-function Classcomponent() {
-  return (
-    <div><p>lasscomponent</p></div>
-  )
-}
-
-export default Classcomponent;
